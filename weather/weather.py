b8619813ad80be5fcb580ccc560ebdda42806471
pavel26102000 <47847079+pavel26102000@users.noreply.github.com>
2020-05-23 17:33:02 +0300

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import xml.etree.ElementTree as ET
 
 from get_weather import get_weather
 from classes import *
@@ -35,53 +36,82 @@ def consider_weather(w_info):
                 all_vehs.append(veh_id)
 
 
-# checks if veh in polygon
-def inPolygon(veh_id, xp, yp):
-    x = traci.vehicle.getPosition(veh_id)[0]
-    y = traci.vehicle.getPosition(veh_id)[1]
-    c = 0
-    for i in range(len(xp)):
-        if (((yp[i] <= y and y < yp[i - 1]) or (yp[i - 1] <= y and y < yp[i])) and \
-                (x > (xp[i - 1] - xp[i]) * (y - yp[i]) / (yp[i - 1] - yp[i]) + xp[i])): c = 1 - c
-
-    return c
+def createWClass(w_type, w_value):
+    if w_type == "snow":
+        return Snow(w_value)
+    elif w_type == "rain":
+        return Rain(w_value)
 
 
-def consider_weather_area(w_info, xp, yp):
+def changeAParams(veh_id, vehs_set, w, area, a_type):
+    a_params = list()
+    ret_val = Vehicle(veh_id, a_type, a_params)
+    if a_type == 'polygon':
+        a_params.append(list([float(i) for i in area.get('x').split(", ")]))
+        a_params.append(list([float(i) for i in area.get('y').split(", ")]))
+    elif a_type == 'circle':
+        a_params.append(float(area.get('c_x')))
+        a_params.append(float(area.get('c_y')))
+        a_params.append(float(area.get('r')))
+    if veh_id not in vehs_set and inArea(veh_id, a_type, a_params):
+        vehs_set.add(veh_id)
+        w.changeParams(veh_id, get_veh_params(veh_id))
+        return ret_val
+    else:
+        return Vehicle("none")
+
+
+def changeWParams(w_type, w_info, veh_id, vehs_set):
+    ret_val = Vehicle("none")
+    for polygon in w_info.findall('polygon'):
+        w = createWClass(w_type, float(polygon.get('value')))
+        if ret_val.id == "none":
+            ret_val = changeAParams(veh_id, vehs_set, w, polygon, 'polygon')
+    for circle in w_info.findall('circle'):
+        w = createWClass(w_type, float(circle.get('value')))
+        if ret_val.id == "none":
+            ret_val = changeAParams(veh_id, vehs_set, w, circle, 'circle')
+    return ret_val
+
+
+def consider_weather_area(w_info):
     all_vehs = set()
-    w_list = list()
     aff_vehs = dllist()
-    for w_type, w_val in w_info:
-        if w_type == "rain":
-            w_list.append(Rain(w_val['value']))
-        if w_type == "snow":
-            w_list.append(Snow(w_val['value']))
+    # for w_type, w_val in w_info:
+    #     if w_type == "rain":
+    #         w_list.append(Rain(w_val['value']))
+    #     if w_type == "snow":
+    #         w_list.append(Snow(w_val['value']))
 
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
         vehs = traci.vehicle.getIDList()
         for veh_id in vehs:
-            if veh_id not in all_vehs and inPolygon(veh_id, xp, yp):
-                aff_vehs.append(Vehicle(veh_id, xp, yp))
-                for w in w_list:
-                    w.changeParams(veh_id, get_veh_params(veh_id))
-                all_vehs.add(veh_id)
+            for snow in w_info.findall('snow'):
+                v = changeWParams('snow', snow, veh_id, all_vehs)
+                if v.id != "none":
+                    aff_vehs.append(v)
+            for rain in w_info.findall('rain'):
+                v = changeWParams('rain', rain, veh_id, all_vehs)
+                if v.id != "none":
+                    aff_vehs.append(v)
         if len(aff_vehs) > 0:
             curr_veh = aff_vehs.last
             for _ in range(len(aff_vehs)):
+                flag = 1
                 temp = curr_veh.prev
-                if not inPolygon(curr_veh.value.id, curr_veh.value.polygon_x,
-                                 curr_veh.value.polygon_y):
+                try:
+                    flag = curr_veh.value.in_area()
+                except traci.exceptions.TraCIException:
+                    aff_vehs.remove(curr_veh)
+                    all_vehs.remove(curr_veh.value.id)
+                if not flag:
                     curr_veh.value.restore_params()
                     aff_vehs.remove(curr_veh)
+                    all_vehs.remove(curr_veh.value.id)
                 curr_veh = temp
 
 
 def weather_main():
-    weather, xp, yp = get_weather('data/weather.xml')
-    print(xp, yp)
-    if xp == -1 and yp == -1:
-        consider_weather(weather.items())
-    else:
-        print('here')
-        consider_weather_area(weather.items(), xp, yp)
+    weather = get_weather('data/weather.xml')
+    consider_weather_area(weather)
