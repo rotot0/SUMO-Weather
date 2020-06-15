45d622d350210db6afe6c82b88db22a16ae94f2d
Akim <akimon55@gmail.com>
2020-06-15 17:40:12 +0300

    readme changes

---
+++ tests/double_cross/data/weather_funcs.py
@@ -0,0 +1,14 @@
+# def SnowChangeAccel(weather_val, param):
+#     return param # do not change acceleration
+
+# def SnowChangeDecel(weather_val, param):
+#     if weather_val < 50:
+#         return param
+#     else:
+#         return param / 2
+
+# def SnowChangeColor(weather_val, color_values):
+#     color_list[0]  = 213
+#     color_list[1]  = 0
+#     color_list[2]  = 110
+#     return color_values
